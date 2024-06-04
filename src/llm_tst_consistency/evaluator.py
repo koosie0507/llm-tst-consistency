cfe8da49c253d402f21d15ea149d7aa94950fde3
Andrei Olar <andrei.olar@dpgmedia.nl>
2024-06-05 00:11:06 +0300

    tweak the prompt

--- src/llm_tst_consistency/evaluator.py
+++ src/llm_tst_consistency/evaluator.py
@@ -2,7 +2,6 @@ import os.path
 from dataclasses import asdict
 from pathlib import Path
 
-import orjson as json
 import pandas as pd
 import spacy
 from datasets import load_dataset
@@ -51,7 +50,69 @@ def render_hlf_instruction(template_name: str, stats: Stats) -> str:
     return template.render(**asdict(stats))
 
 
-INPUT = "Interesting fact: by the year 2020 all actors on american tv shows will be australian."
+INPUT = """
+Young people with internet addiction experience changes in their brain chemistry
+which could lead to more addictive behaviours, research suggests.
+
+The study, published in PLOS Mental Health, reviewed previous research using
+functional magnetic resonance imaging (fMRI) to examine how regions of the brain
+interact in people with internet addiction.
+
+They found that the effects were evident throughout multiple neural networks in
+the brains of young people, and that there was increased activity in parts of
+the brain when participants were resting.
+
+At the same time, there was an overall decrease in the functional connectivity
+in parts of the brain involved in active thinking, which is the executive
+control network of the brain responsible for memory and decision-making.
+
+The research found that these changes resulted in addictive behaviours and
+tendencies in adolescents, as well as behavioural changes linked to mental
+health, development, intellectual ability and physical coordination.
+
+The researchers reviewed 12 previous studies involving 237 10- to 19-year-olds
+with a formal diagnosis of internet addiction between 2013 and 2023.
+
+Almost half of British teenagers have said they feel addicted to social media,
+according to a survey this year.
+
+Max Chang, the study’s lead author and an MSc student at the UCL Great Ormond
+Street Institute of Child Health (GOS ICH), said: “Adolescence is a crucial
+developmental stage during which people go through significant changes in their
+biology, cognition and personalities.
+
+“As a result, the brain is particularly vulnerable to internet addiction-related
+urges during this time, such as compulsive internet usage, cravings towards
+usage of the mouse or keyboard and consuming media.
+
+“The findings from our study show that this can lead to potentially negative
+behavioural and developmental changes that could impact the lives of
+adolescents. For example, they may struggle to maintain relationships and social
+activities, lie about online activity and experience irregular eating and
+disrupted sleep.”
+
+Chang added that he hoped the findings demonstrated “how internet addiction
+alters the connection between the brain networks in adolescence”, which would
+then allow early signs of internet addiction to be treated effectively.
+
+He added: “Clinicians could potentially prescribe treatment to aim at certain
+brain regions or suggest psychotherapy or family therapy targeting key symptoms
+of internet addiction.
+
+“Importantly, parental education on internet addiction is another possible
+avenue of prevention from a public health standpoint. Parents who are aware of
+the early signs and onset of internet addiction will more effectively handle
+screen time, impulsivity, and minimise the risk factors surrounding internet
+addiction.”
+
+Irene Lee, a senior author of the research paper also based at GOS ICH, said:
+“There is no doubt that the internet has certain advantages. However, when it
+begins to affect our day-to-day lives, it is a problem.
+
+“We would advise that young people enforce sensible time limits for their daily
+internet usage and ensure that they are aware of the psychological and social
+implications of spending too much time online.”
+"""
 
 
 if __name__ == "__main__":
